355d721c21c26c2cee4ecf745ebf8ee232f89193
Christina Loiacono <loiaconochristina@gmail.com>
2021-10-21 19:50:27 -0400

    created one component using React

---
+++ src/App.js
@@ -0,0 +1,11 @@
+/* Create a component - A file that holds all the logic. Html, styling, logic. */
+import React from 'react'; 3.4K
+
+/* Create component by writing a function */
+function App(){
+  return(
+    <div>
+      <h1>Hello React</h1>
+    </div>
+  )
+}

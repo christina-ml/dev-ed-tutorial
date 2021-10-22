b2a5e7162ea8c216b354bdb4345460d32fe1463d
Christina Loiacono <loiaconochristina@gmail.com>
2021-10-22 15:58:25 -0400

    adding State. importe useState. Then create an Increment, and count.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 /* Create a component - A file that holds all the logic. Html, styling, logic. */
-import React from 'react';
+import React, {useState} from 'react';
 import Tweet from './Tweet';
 
 /*
 Create component by writing a function
 */
 function App(){
+
+  const [isRed, setRed] = useState(false);
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  }
+
   return(
     <div className="app">
-      <Tweet name="Dev ED" message="This is a random tweet"/>
-      <Tweet name="John Snow" message="I am the true king" />
-      <Tweet name="Traversy Media" message="700k my dudes" />
-      <Tweet name="Mosh" message="My new course is available" />
+      <button onClick={increment}>Increment</button>
+      <h1>{count}</h1>
     </div>
   )
 }
@@ -24,4 +30,14 @@ export default App;
 - in our index.js we have import React from 'react'; 3.4K again
 - and a import ReactDOM, and a ReactDOM.render(<App />) compoment; and then getElementById on 'root'
 This isn't HTML, it's JSX which is essentially just JavaScript
+*/
+
+
+/*
+Deleted the Tweet, and will get back to it later.
+
+      <Tweet name="Dev ED" message="This is a random tweet"/>
+      <Tweet name="John Snow" message="I am the true king" />
+      <Tweet name="Traversy Media" message="700k my dudes" />
+      <Tweet name="Mosh" message="My new course is available" />
 */

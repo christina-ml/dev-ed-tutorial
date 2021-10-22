9a21f26da3666256c172106d0bb04e5cf0f87909
Christina Loiacono <loiaconochristina@gmail.com>
2021-10-22 14:39:32 -0400

    made sayHello component. But it's unrealistic so we will delete it.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 /* Create a component - A file that holds all the logic. Html, styling, logic. */
-import React from 'react'; 3.4K
+import React from 'react';
+import Hello from './sayHello';
 
-/* Create component by writing a function */
+/*
+Create component by writing a function
+*/
 function App(){
   return(
     <div>
-      <h1>Hello React</h1>
+      <h1>This is the app component</h1>
+      <Hello />
     </div>
   )
 }
+
+export default App;
+
+/*
+- Create a component
+- Export it
+- in our index.js we have import React from 'react'; 3.4K again
+- and a import ReactDOM, and a ReactDOM.render(<App />) compoment; and then getElementById on 'root'
+This isn't HTML, it's JSX which is essentially just JavaScript
+*/

13315a43264aa6011121045ecdbc6227c4036403
Christina Loiacono <loiaconochristina@gmail.com>
2021-10-22 14:48:05 -0400

    added a className to Tweet.js and imported the App.css

--- src/tweet.js
+++ src/tweet.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import './App.css';
 
+/* You have to use `className` not `class` to add the id, because we are in JavaScript. Not HTML. */
 function Tweet(){
     return (
-        <div>
+        <div className="tweet">
             <h3>Name</h3>
             <p>This is a random tweet</p>
             <h3>Number of likes</h3>

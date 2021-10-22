3decd915a54cb7063b43e4d77b5e526e02259d41
Christina Loiacono <loiaconochristina@gmail.com>
2021-10-22 15:53:04 -0400

    destructured props parameter in Tweet.js using es6

--- src/tweet.js
+++ src/tweet.js
@@ -2,11 +2,11 @@ import React from 'react';
 import './App.css';
 
 /* You have to use `className` not `class` to add the id, because we are in JavaScript. Not HTML. */
-function Tweet(props){
+function Tweet({name, message}){
     return (
         <div className="tweet">
-            <h3>{props.name}</h3>
-            <p>{props.message}</p>
+            <h3>{name}</h3>
+            <p>{message}</p>
             <h3>Number of likes</h3>
         </div>
     );
